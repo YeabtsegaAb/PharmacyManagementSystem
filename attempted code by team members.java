00fd978b6be93d96c9bb7ff3fc9f6b719fa798a0
Yohannes Mulugeta <yohannes.mulugeta319@gmail.com>
2025-08-05 11:31:56 +0300

    Add simple Java test file

---
+++ attempted code by team members.java
@@ -0,0 +1,18 @@
+// HelloWorld.java
+// This is a simple, independent Java program.
+// It does not depend on any other classes in your project.
+
+public class HelloWorld {
+
+    /**
+     * The main method is the entry point for any Java application.
+     * When you run this file, this is the first method that gets executed.
+     *
+     * @param args Command line arguments (not used in this example).
+     */
+    public static void main(String[] args) {
+        // System.out.println() is used to print output to the console.
+        System.out.println("Hello from a new Java file on GitHub!");
+        System.out.println("This file is just for testing commits and pushes.");
+    }
+}
